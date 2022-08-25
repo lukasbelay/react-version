ce3c7d7c7a3401b61a72dd7e574e0c929f4e1158
lukas <lukasbelay87@gmail.com>
2022-08-25 15:39:01 -0700

    better comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { Clock } from "three";
 import "./App.css";
 
 export default function App() {
+  //useEffect because all this comes after canvas loads
   useEffect(() => {
+    //setting up texture loader for future use
     const textureLoader = new THREE.TextureLoader();
     const normalTexture = textureLoader.load("height2.jpg");
-
+    //setting up scene , camera and renderer
     let Scene = new THREE.Scene();
     let camera = new THREE.PerspectiveCamera(
       75,
@@ -15,36 +17,35 @@ export default function App() {
       0.1,
       1000
     );
-
+    //appending renderer to canvas
     let renderer = new THREE.WebGLRenderer({
       canvas: document.getElementById("bg"),
       antialias: "true",
     });
+    //initializing clock for later
     let Clock = new THREE.Clock();
+    //initializing renderer and camera
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.position.setZ(30);
     camera.position.setX(-3);
     renderer.render(Scene, camera);
-
+    //setting up lighting
     const pointLight = new THREE.AmbientLight(0xffffff);
     pointLight.position.set(0, 0, 0);
 
     Scene.add(pointLight);
-
+    //using texture loader
     const spaceTexture = new THREE.TextureLoader().load("space.jpg");
     Scene.background = spaceTexture;
 
     const moonTexture = new THREE.TextureLoader().load("moon.jpg");
-
+    //initializing thigs as objects
     let moon = new THREE.Mesh(
       new THREE.SphereGeometry(7, 64, 32),
       new THREE.MeshStandardMaterial({
         map: moonTexture,
         normalMap: normalTexture,
-        /*metalness: 0.2,
-      roughness: 0.9,
-      */
       })
     );
 
@@ -53,14 +54,11 @@ export default function App() {
       new THREE.MeshStandardMaterial({
         map: moonTexture,
         normalMap: normalTexture,
-        /*metalness: 0.2,
-        roughness: 0.9,
-        */
       })
     );
-
+    //adding objects
     Scene.add(moon, moonNew);
-
+    //setting locations
     moon.position.x = 27;
     moon.position.z = -5;
     moonNew.position.x = -30;
@@ -68,26 +66,20 @@ export default function App() {
 
     function rotate() {
       requestAnimationFrame(rotate);
-
-      //controls.update();
       renderer.render(Scene, camera);
-      //stats.update();
     }
 
     rotate();
+    //this updates the renderer when the browser changes
     function onWindowResize() {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
       renderer.setSize(window.innerWidth, window.innerHeight);
     }
     window.addEventListener("resize", () => onWindowResize(), false);
-
+    //this loops the rendering
     function animate() {
       requestAnimationFrame(animate);
-
-      //torus.rotation.x += 6.5;
-      //torus.rotation.y += 6;
-      //torus.rotation.z += 6;
       let time = Clock.getElapsedTime();
       moonNew.position.x = Math.cos(time) * 10;
       moonNew.position.y = Math.sin(time) * 10;
@@ -99,7 +91,7 @@ export default function App() {
     }
 
     animate();
-
+    //this loops everything
     THREE.LoopRepeat;
   });
 
